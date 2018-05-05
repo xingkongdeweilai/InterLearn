07bf354e4774e3ea5e0a9167f1202c8f33df4f41
星空的未来 <1450235636@qq.com>
2018-05-05 23:20:16 +0800

    心态崩了

---
+++ src/main/java/edu/cqut/llj/controller/HelloController.java
@@ -0,0 +1,36 @@
+package edu.cqut.llj.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import edu.cqut.llj.vo.GirlProperties;
+
+@Controller
+@ResponseBody
+@RequestMapping({"/hello","/hi"})
+public class HelloController {
+	
+//	@Value("${cupSize}")
+//	private String cupSize;
+	
+//	@Value("${age}")
+//	private Integer age;
+	
+//	@Autowired
+//	private GirlProperties girlProperties;
+	
+	@GetMapping(value="/say")
+	public String say(@RequestParam(value="id",required=false,defaultValue="0") Integer id){
+		return "id: "+id;
+	}
+	
+//	@GetMapping(value="/say/{id}")
+//	public String say(@PathVariable Integer id){
+//		return girlProperties.getCupSize();
+//		return "index";
+//		return "id: "+id;
+//	}
+
+}
