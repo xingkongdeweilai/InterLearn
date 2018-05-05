32c530ba119858ec59485453fb4d6f975f928d13
星空的未来 <1450235636@qq.com>
2018-05-05 20:12:30 +0800

    正式提交

---
+++ src/main/java/edu/cqut/llj/InterLearnApplication.java
@@ -0,0 +1,15 @@
+package edu.cqut.llj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * spring boot启动
+ * @author Administrator
+ *
+ */
+@SpringBootApplication
+public class InterLearnApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(InterLearnApplication.class, args);
+	}
+}
---
+++ src/test/java/edu/cqut/llj/InterLearnApplicationTest.java
@@ -0,0 +1,16 @@
+package edu.cqut.llj;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InterLearnApplicationTest {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
